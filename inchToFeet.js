c70d5b06dfe004ce7849602148ea56815e7e45c2
ashraf-git <aliashraf.hena@gmail.com>
2020-07-24 18:06:30 +0600

    apply-javascript

---
+++ inchToFeet.js
@@ -0,0 +1,17 @@
+// var inch = 156;
+// var feet = inch/12;
+// console.log(feet); 
+// console.log(inch); 
+// console.log(inch); 
+
+function inchToFeet(inch){
+    var feet = inch/12;
+    return feet;
+}
+var senior = [234, 342, 423];
+var nanaFeet = inchToFeet(senior[0])
+// var nanaFeet = inchToFeet(500);
+console.log(nanaFeet);
+var naniFeet = inchToFeet(senior[1]);
+console.log(naniFeet);
+
